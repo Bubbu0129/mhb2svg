9806db667f0553b633751c9708773bbc7e80abc4
Casper Dong <bubbu0129@gmail.com>
2025-12-13 17:10:28 +0800

    feat: paging

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 ```
 $ python main.py --help
-usage: main.py [-h] [-c] (-f FILE | -l LINK) [-p PADDING] [-r RATIO]
+usage: main.py [-h] [-c] [-p] (-f FILE | -l LINK)
 
 options:
-  -h, --help            show this help message and exit
-  -c, --color           Enable color (default black & white)
-  -f, --file FILE       Path to .mhb file
-  -l, --link LINK       MAXHUB URL containing the s_id argument
-  -p, --padding PADDING Padding size for .svg (integer)
-  -r, --ratio RATIO     Stoke width ratio (float)
+  -h, --help       show this help message and exit
+  -c, --color      Enable color (default black & white)
+  -p, --paging     Enable paging (default none)
+  -f, --file FILE  Path to .mhb file
+  -l, --link LINK  MAXHUB URL containing the s_id argument
 ```
--- main.py
+++ main.py
@@ -9,58 +9,68 @@ import tempfile
 
 BG_color = '#363b41' # Background color of the slides
 API_tpl = 'https://res.maxhub.com/v3/clientairdisk/api/share/v2/{sid}/resources.json' # API template
+aspect_ratio = 1.414 # sqrt(2), A series
+padding = 10.0
+stroke_ratio = 1.0
 
-def convert(input_path, output_path, isColor, padding, ratio):
+def write_svg(min_x, min_y, width, height, strokes, output_path, color):
+    with open(output_path, 'w') as f:
+        f.write(
+            f'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 {width + 2 * padding} {height + 2 * padding}">\n'
+        )
+        if color: # Render fullscreen rectangle with background color
+            f.write(
+                f'<rect x="0" y="0" width="{width + 2 * padding}" height="{height + 2 * padding}" fill="{BG_color}"/>\n'
+            )
+        for stroke in strokes:
+            if min_y < stroke["points"][0][1] < min_y + height:
+                points_str = ' '.join([f'{x - min_x + padding},{y - min_y + padding}' for x, y in stroke["points"]])
+                f.write(f'<polyline points="{points_str}" fill="none" stroke="{stroke["color"]}" stroke-width="{stroke["width"]}"/>\n')
+        f.write('</svg>\n')
+
+def convert(input_path, output_prefix, color, paging):
     tree = ET.parse(input_path)
     root = tree.getroot()
     lines = []
     min_x, min_y = float('inf'), float('inf')
     max_x, max_y = float('-inf'), float('-inf')
+    strokes = []
     for ink in root.findall('.//Ink'):
-        width_str = ink.findtext('Thickness')
-        if width_str:
-            width = float(width_str)
+        stroke_width_str = ink.findtext('Thickness')
+        if stroke_width_str:
+            stroke_width = float(stroke_width_str)
         else:
-            width = 1.0
-        width *= ratio
-        if isColor:
-            color = ink.findtext('ForegroundColor')
+            stroke_width = 1.0
+        stroke_width *= stroke_ratio
+        if color:
+            stroke_color = ink.findtext('ForegroundColor')
         else:
-            color = '#000000'
+            stroke_color = '#000000'
         points = ink.find('Points')
         if points is None:
             continue
-        stroke = []
+        stroke = {"points": [], "color": stroke_color, "width": stroke_width}
         for pt in points.findall('StylusPoint'):
             # Format: 'x,y,pressure'
             parts = pt.text.split(',')
             x = float(parts[0])
             y = float(parts[1])
-            stroke.append((x, y))
             if x < min_x: min_x = x
             if x > max_x: max_x = x
             if y < min_y: min_y = y
             if y > max_y: max_y = y
-        points_str = ' '.join([f'{x},{y}' for x, y in stroke])
-        lines.append(
-            f'<polyline points="{points_str}" '
-            f'fill="none" stroke="{color}" stroke-width="{width}"/>\n'
-        )
-    viewbox_width = max_x - min_x + (padding * 2)
-    viewbox_height = max_y - min_y + (padding * 2)
-    viewbox_x = min_x - padding
-    viewbox_y = min_y - padding
-    with open(output_path, 'w') as f:
-        f.write(
-            f'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="{viewbox_x} {viewbox_y} {viewbox_width} {viewbox_height}">\n'
-        )
-        if isColor: # Render fullscreen rectangle with background color
-            f.write(
-                f'<rect x="{viewbox_x}" y="{viewbox_y}" width="{viewbox_width}" height="{viewbox_height}" fill="{BG_color}"/>\n'
-            )
-        f.writelines(lines)
-        f.write('</svg>\n')
-    return
+            stroke["points"].append((x, y))
+        strokes.append(stroke)
+    width = max_x - min_x
+    height = max_y - min_y
+    pages = int(height / width / aspect_ratio)
+    if pages == 0 or not paging:
+        write_svg(min_x, min_y, width, height, strokes, output_prefix + '.svg', color)
+        return 0
+    dh = (height - aspect_ratio * width) / pages
+    for i in range(pages + 1):
+        write_svg(min_x, min_y + i * dh, width, aspect_ratio * width, strokes, f"{output_prefix}-{i}.svg", color)
+    return pages
 
 def extract_sid(url):
     parsed_url = urllib.parse.urlparse(url)
@@ -100,9 +110,8 @@ def extract_archive(file_path, dst_path):
             zip_ref.extractall(dst_path)
     except zipfile.BadZipFile:
         raise RuntimeError('The downloaded file is not a valid archive.')
-    return
 
-def process_slides(src_path, color, padding, ratio):
+def process_slides(src_path, color, paging):
     slides_dir = src_path / 'Slides'
     if not slides_dir.exists():
         print(f'Warning: {slides_dir} does not exist. Skipping conversion.')
@@ -112,19 +121,21 @@ def process_slides(src_path, color, padding, ratio):
         print('No XML files found in Slides directory.')
     slides = []
     for xml_path in xml_files:
-        output_path = f'{xml_path.stem}.svg'
-        convert(str(xml_path), output_path, color, padding, ratio)
-        slides.append(output_path)
+        output_prefix = xml_path.stem
+        pages = convert(str(xml_path), output_prefix, color, paging)
+        if pages == 0:
+            slides.append(output_prefix)
+        else:
+            slides.append(f"{output_prefix} ({pages})")
     return slides
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--color', action='store_true', help='Enable color (default black & white)')
+    parser.add_argument('-c', '--color',  action='store_true', help='Enable color (default black & white)')
+    parser.add_argument('-p', '--paging', action='store_true', help='Enable paging (default none)')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-f', '--file', type=str, help='Path to .mhb file')
     group.add_argument('-l', '--link', type=str, help='MAXHUB URL containing the s_id argument')
-    parser.add_argument('-p', '--padding', type=int, default=10, help='Padding size for .svg (integer)')
-    parser.add_argument('-r', '--ratio', type=float, default=1.0, help='Stoke width ratio (float)')
     return parser.parse_args()
 
 def parse_metadata(src_path):
@@ -153,6 +164,6 @@ if __name__ == '__main__':
         for tag, text in metadata:
             print(f'\t{tag}: {text}')
         print('Processing slides...')
-        slides = process_slides(tmp_path, args.color, args.padding, args.ratio)
+        slides = process_slides(tmp_path, args.color, args.paging)
         print(f'Generated ' + ', '.join(slides))
 
